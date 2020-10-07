5c985df994a76de8981b2874fafc51b65dd39f03
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-10-07 20:45:03 +0000

    modified:   conditionals_rps.rb all done 100%

--- conditionals_rps.rb
+++ conditionals_rps.rb
@@ -7,3 +7,19 @@
 # If you need a refresher on the rules of Rock, Paper, Scissors: https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors
 
 p "Please choose rock, paper, or scissors:"
+
+choice = gets.chomp.downcase
+
+if choice == "rock"
+  p "You played rock!" 
+  p "The computer played scissors!"
+  p "You won!"
+elsif choice == "paper" 
+  p "You played paper!" 
+  p "The computer played scissors!"
+  p "You lost!"
+elsif choice == "scissors"
+  p "You played scissors!"
+  p "The computer played scissors!"
+  p "You tied!"
+end
